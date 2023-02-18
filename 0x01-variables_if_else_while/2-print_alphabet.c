6a54fcaf3ec0c97a575dd10200e686f84c046991
Marwan Abdo <Marwanabdoismail.com>
2023-02-17 17:48:40 -0800

    S

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar(alp[i]);
+}
+putchar("\n")
+return (0);
+}
